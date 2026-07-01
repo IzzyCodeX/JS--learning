6bc888d745323907a39c0cd6cb6e6c2f933c6c83
Izzy <adelusiisrael87@gmail.com>
2026-07-01 23:21:02 +0100

    Add Exercise.js with basic functions and variables

---
+++ Exercise.js
@@ -0,0 +1,25 @@
+let string = "Hello, World!";
+let number = 39;
+let isActive = true;
+let mySelf = {
+    name: "Israel",
+    hobby: "Coding",
+    color: "blue",
+    title: "Mr"
+};
+let goodnights = ["Gute Nacht", "Bonne nuit", "Buenas noches", "Oyasumi"];
+
+
+
+function takeSum(a, b){
+    return a + b ;
+}
+function takeProduct(a, b){
+    return a * b ;
+}
+function takeDifference(a, b){
+    return a - b;
+}
+console.log(takeSum(3, 6))
+console.log(takeProduct(4, 6))
+console.log(takeDifference(9, 6))
