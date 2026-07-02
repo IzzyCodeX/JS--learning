0770937d66449ff55322793a38b230a04ced7a4b
IzzyCodeX <adelusiisrael87@gmail.com>
2026-07-02 04:53:48 +0100

    2 and 3

--- Exercise.js
+++ Exercise.js
@@ -23,3 +23,15 @@ function takeDifference(a, b){
 console.log(takeSum(3, 6))
 console.log(takeProduct(4, 6))
 console.log(takeDifference(9, 6))
+
+function greetUser(name){
+    return `Hello, ${name}! Welcome to JavaScript programming!`;
+}
+ console.log(greetUser("Israel"))
+
+ 
+ function logDataType(value){
+    return typeof value;
+ }
+console.log(logDataType(true))
+
