6fd86b4996e147dc0f1716d8c7a585b49a0d2618
IzzyCodeX <adelusiisrael87@gmail.com>
2026-07-09 12:47:20 +0100

    through with it

--- app.js
+++ app.js
@@ -830,7 +830,11 @@ function golfScore (par, strokes){
     } else if (strokes == par){
         return names[3];
     } else if (strokes == par +1){
-        
+        return names[4];
+    } else if (strokes == par +2){
+        return names[5];
+    } else {
+        return names[6];
     }
 }
 
